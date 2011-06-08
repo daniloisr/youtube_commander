8f3b585bff5a2d063ee9508388264809cc20e283
Kássio Borges de Melo <kassioborgesm@gmail.com>
2011-06-08 15:34:09 -0300

    started work to control many youtube tabs

--- TODO.rb
+++ TODO.rb
@@ -1,4 +1,6 @@
 # FIXME: Controlar videos em paginas de canais
+# TODO: Quando a aba do youtube recarregar enviar os dados para o Commander
+# TODO: Colocar o icone em pause/play de acordo com a ação que está ocorrendo
 # TODO: Barra de progressão do video
 # TODO: Miniatura do video
 # TODO: Controlar várias abas do youtube
--- commander.js
+++ commander.js
@@ -1,90 +1,93 @@
 chrome.extension.onRequest.addListener( function(request, sender, sendResponse) {
-    player = new Player();
-    console.dir(player.getStats());
-    eval("player."+request.command+"("+request.params+")");
-    sendResponse(player.getStats());
+	player = new Player();
+	console.dir(player.getStats());
+	eval("player."+request.command+"("+request.params+")");
+	sendResponse(player.getStats());
 });
 
 Player = function(){
-    this.player = null;
-    this.type = null;
+	this.player = null;
+	this.type = null;
 
-    if(document.getElementById("movie_player")) {
-        this.player = document.getElementById("movie_player");
-        this.type = "flash";
-    } else if(document.getElementsByClassName("video-stream")[0]) {
-        this.player = document.getElementsByClassName("video-stream")[0];
-        this.type = "html5";
-    }
+	if(document.getElementById("movie_player")) {
+		this.player = document.getElementById("movie_player");
+		this.type = "flash";
+	} else if(document.getElementsByClassName("video-stream")[0]) {
+		this.player = document.getElementsByClassName("video-stream")[0];
+		this.type = "html5";
+	}
 
-    this.play_pause = function() {
-        if(this.type == "flash") {
-            if(this.player.getPlayerState() == 1) {
-                this.player.pauseVideo();
-            } else if(this.player.getPlayerState() == 2) {
-                this.player.playVideo();
-            }
-        } else if(this.type == "html5") {
-            if(this.player.paused) {
-                this.player.play();
-            } else {
-                this.player.pause();
-            }
-        }
-        return this;
-    }
+	console.dir(this.player);
 
-    this.volume = function(value) {
-        if(this.type == "flash") {
-            this.player.setVolume(value);
-        } else if(this.type == "html5") {
-            this.player.volume = Math.round(value/100);
-        }
+	this.play_pause = function() {
+		if(this.type == "flash") {
+			if(this.player.getPlayerState() == 1) {
+				this.player.pauseVideo();
+			} else if(this.player.getPlayerState() == 2) {
+				this.player.playVideo();
+			}
+		} else if(this.type == "html5") {
+			if(this.player.paused) {
+				this.player.play();
+			} else {
+				this.player.pause();
+			}
+		}
+		return this;
+	}
 
-        return this;
-    }
+	this.volume = function(value) {
+		if(this.type == "flash") {
+			this.player.setVolume(value);
+		} else if(this.type == "html5") {
+			this.player.volume = Math.round(value/100);
+		}
 
-    this.mute = function() {
-        if(this.type == "flash") {
-            if(this.player.isMuted()) {
-                this.player.unMute();
-            } else {
-                this.player.mute()
-            } 
-        } else if(this.type == "html5") {
-            this.player.muted = !player.muted;
-        }
-        return this;
-    }
+		return this;
+	}
 
-    this.getStats = function(){
-        var stats = new Object();
+	this.mute = function() {
+		if(this.type == "flash") {
+			if(this.player.isMuted()) {
+				this.player.unMute();
+			} else {
+				this.player.mute()
+			} 
+		} else if(this.type == "html5") {
+			this.player.muted = !player.muted;
+		}
+		return this;
+	}
 
-        if(this.type == "flash") {
-            stats.mute = this.player.isMuted();
-        } else if(this.type == "html5") {
-            stats.mute = this.player.muted;
-        }
+	this.getStats = function(){
+		var stats = new Object();
 
-        if(this.type == "flash") {
-            stats.volume = this.player.getVolume();
-        } else if(this.type == "html5") {
-            stats.volume = this.player.volume;
-        }
+		if(this.type == "flash") {
+			stats.mute = this.player.isMuted();
+		} else if(this.type == "html5") {
+			stats.mute = this.player.muted;
+		}
 
-        if(this.type == "flash") {
-            if(this.player.getPlayerState() == 1) {
-                stats.play = true;
-            } else if(this.player.getPlayerState() == 2) {
-                stats.play = false;
-            }
-        } else if(this.type == "html5") {
-            stats.play = !this.player.paused;
-        }
-        stats.type = this.type;
-        stats.title = document.getElementById("eow-title").title;
-        return stats;
-    }
+		if(this.type == "flash") {
+			stats.volume = this.player.getVolume();
+		} else if(this.type == "html5") {
+			stats.volume = this.player.volume;
+		}
 
-    return this;
+		if(this.type == "flash") {
+			if(this.player.getPlayerState() == 1) {
+				stats.play = true;
+			} else if(this.player.getPlayerState() == 2) {
+				stats.play = false;
+			}
+		} else if(this.type == "html5") {
+			stats.play = !this.player.paused;
+		}
+		stats.type = this.type;
+		stats.title = document.getElementById("eow-title").title;
+		stats.video_id = this.player.baseURI.match(/v=([^&]*)/)[1];
+		return stats;
+	}
+
+	return this;
 }
