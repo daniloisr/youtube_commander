c46522daff139be8ce0738b7490ef8613883cfa6
Kássio Borges de Melo <kassioborgesm@gmail.com>
2011-06-07 22:10:27 -0300

    cleaning the enviornment, updating images.

---
+++ TODO.rb
@@ -0,0 +1,7 @@
+# FIXME: Controlar videos em paginas de canais
+# TODO: Barra de progressão do video
+# TODO: Miniatura do video
+# TODO: Controlar várias abas do youtube
+# TODO: Controlar videos em abas que não sejam do youtube
+# TODO: Controlar fila de videos
+# TODO: Notificações de mudanças de músicas(opcional)
